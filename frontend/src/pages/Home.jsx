372f01b34ba5d16d58b92ca5d03b3f95199cb2de
Ajay Maurya <ajay.webd@gmail.com>
2024-11-21 15:37:59 +0530

    may be some changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,12 +3,10 @@ import FeaturesSection from "../components/FeaturesSection";
 // import ServicesSection from "../components/ServicesSection";
 import NewsSection from "../components/NewsSection";
 // import FAQ from "../components/FAQ";
-import Testimonial from "../components/Testimonial";
+// import Testimonial from "../components/Testimonial";
 import SliderComponent from "../components/SliderComponent";
 import CarouselSlider from "../components/CarouselSlider";
 import StatsComponent from "../components/StatsComponent";
-import AllNews from "../components/AllNews";
-
 const Home = () => {
   return (
     <>
